e9b8b91a0bd529a4c2016206928c393169eb10bd
Alonso202 <drosk1711@gmail.com>
2022-05-09 19:03:48 -0400

    Se añadio imagen

--- MusicPro/apiProductos/models.py
+++ MusicPro/apiProductos/models.py
@@ -9,6 +9,7 @@ class Producto (models.Model):
     nombre = models.CharField(max_length=30)
     fecha = models.DateTimeField(default=timezone.now)
     valor = models.IntegerField()
+    imagen = models.ImageField(upload_to='img', null=True)
     def __str__(self) :
         return self.nombre
    
